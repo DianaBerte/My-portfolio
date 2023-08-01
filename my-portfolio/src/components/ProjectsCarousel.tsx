f6eb7790400c09000e361df4960aaa64d96bc19a
DianaBerte <dianaberte.go@gmail.com>
2023-08-01 17:11:36 +0200

    Styling carousel's arrows

--- my-portfolio/src/components/ProjectsCarousel.tsx
+++ my-portfolio/src/components/ProjectsCarousel.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { FaChevronRight } from 'react-icons/fa'
 import { FaChevronLeft } from 'react-icons/fa'
+import { ICustomArrowStyles } from '../interfaces/ICustomArrowStyles'
 
-const ProjectsCarousel = () => {
+const ProjectsCarousel: React.FC = () => {
     const [hoveredIndex, setHoveredIndex] = useState(null)
 
     const handleMouseEnter = (index: any) => {
@@ -15,12 +17,26 @@ const ProjectsCarousel = () => {
         setHoveredIndex(null);
     };
 
+    const customArrowStyles: ICustomArrowStyles = {
+        top: "50%",
+        transform: "translateY(-50%)",
+        zIndex: 10,
+        backgroundColor: "transparent",
+        color: "#000",
+        fontSize: "2rem",
+        padding: "0.5rem",
+        cursor: "pointer",
+        transition: "color 0.3s ease-in-out",
+      };
+
     const renderCustomArrowPrev = (onClickHandler: any, hasNext: any, label: any) => 
     hasNext && (
         <button
+        className='customArrowPrev'
         type='button'
         onClick={onClickHandler}
         title={label}
+        style={customArrowStyles}
         onMouseEnter={() => handleMouseEnter(hoveredIndex)}
         onMouseLeave={handleMouseLeave}
         >
@@ -31,9 +47,11 @@ const ProjectsCarousel = () => {
     const renderCustomArrowNext = (onCLickHandler: any, hasNext: any, label: any) => 
     hasNext && (
         <button
+        className='customArrowNext'
         type='button'
         onClick={onCLickHandler}
         title={label}
+        style={customArrowStyles}
         onMouseEnter={() => handleMouseEnter(hoveredIndex)}
         onMouseLeave={handleMouseLeave}
         >
@@ -42,11 +60,14 @@ const ProjectsCarousel = () => {
     )
 
     return(
-        <Carousel
-        className='mb-10'
+        <Container >
+            <Row>
+                <Col >
+        <Carousel 
+        className='carousel'
         showArrows={true}
         showThumbs={false}
-        showIndicators={false}
+        showIndicators={true}
         showStatus={false}
         renderArrowPrev={renderCustomArrowPrev}
         renderArrowNext={renderCustomArrowNext}
@@ -59,33 +80,40 @@ const ProjectsCarousel = () => {
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={handleMouseLeave}
             >bla
-            <img src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
+            <img className='carousel-img' src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
             </div>
+
             <div
             onMouseEnter={() => handleMouseEnter(1)}
             onMouseLeave={handleMouseLeave}
             >bla
-            <img src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
+            <img className='carousel-img' src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
             </div>
+
             <div
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={handleMouseLeave}
             >bla
-            <img src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
+            <img className='carousel-img' src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
             </div>
+
             <div
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={handleMouseLeave}
             >bla
-            <img alt='Carousel img' />
+            <img className='carousel-img' src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
             </div>
+
             <div
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={handleMouseLeave}
             >bla
-            <img src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
+            <img className='carousel-img' src='https://www.pngkey.com/png/detail/233-2332677_ega-png.png' alt='Carousel img' />
             </div>
         </Carousel>
+        </Col>
+        </Row>
+        </Container>
     )
 }
 
---
+++ my-portfolio/src/interfaces/ICustomArrowStyles.ts
@@ -0,0 +1,11 @@
+export interface ICustomArrowStyles {
+    top: string;
+    transform: string;
+    zIndex: number;
+    backgroundColor: string;
+    color: string;
+    fontSize: string;
+    padding: string;
+    cursor: string;
+    transition: string;
+}
