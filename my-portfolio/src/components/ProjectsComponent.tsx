8dbcd68941cec4b414bf6639592a72c9d2e8e8dd
DianaBerte <dianaberte.go@gmail.com>
2023-08-07 13:04:39 +0200

    Update ProjectsComponent.tsx

--- my-portfolio/src/components/ProjectsComponent.tsx
+++ my-portfolio/src/components/ProjectsComponent.tsx
@@ -12,8 +12,9 @@ const ProjectsComponent = () => {
         <div id="projects" className="container-div">
             <h1 className="title">Projects</h1>
         </div>
-
+        <h6 style={{color: "whitesmoke"}}>MandarinApp, my first solo project, is a full stack application for practicing Mandarin Chinese</h6>
         <ProjectsCarousel />
+        <h6 style={{color: "whitesmoke"}}>Other teamwork projects include a LinkedIn replica: Front-end and Back-end.</h6>
         
         </Container>
         <FooterComponent />
